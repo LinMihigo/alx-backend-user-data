ff671025c1b4b9cdc2c1ef97361039ad98b971e0
LinMihigo <linmihigo@gmail.com>
2025-06-06 18:30:21 +0200

    Task 8 - 1st fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -41,9 +41,14 @@ class Auth:
     def valid_login(self, email: str, password: str) -> bool:
         """Validate user login credentials
         """
+        user = None
         try:
             user = self._db.find_user_by(email=email)
-            hashed_bytes = user.hashed_password.encode("utf-8")
-            return bcrypt.checkpw(password.encode("uft-8"), hashed_bytes)
-        except Exception:
+            if user is not None:
+                return bcrypt.checkpw(
+                    password.encode("utf-8"),
+                    user.hashed_password.encode("utf-8"),
+                )
+        except NoResultFound:
             return False
+        return False
